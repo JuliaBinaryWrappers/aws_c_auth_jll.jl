3eb46a909474616149314d04221acd91b97fe3f9
jlbuild <juliabuildbot@gmail.com>
2023-09-17 23:57:24 +0000

    aws_c_auth_jll build 0.7.3+0

---
+++ src/aws_c_auth_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule aws_c_auth_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("aws_c_auth")
+JLLWrappers.@generate_main_file("aws_c_auth", UUID("2b3700d1-4306-52e2-a478-c162f0c514be"))
+end  # module aws_c_auth_jll
