cdd9539bde9c6b1c16012f78a72af69b13903b33
jlbuild <juliabuildbot@gmail.com>
2025-12-01 23:52:12 +0000

    aws_c_auth_jll build 0.9.3+0

--- src/aws_c_auth_jll.jl
+++ src/aws_c_auth_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("aws_c_auth")
-JLLWrappers.@generate_main_file("aws_c_auth", UUID("2b3700d1-4306-52e2-a478-c162f0c514be"))
+JLLWrappers.@generate_main_file("aws_c_auth", Base.UUID("2b3700d1-4306-52e2-a478-c162f0c514be"))
 end  # module aws_c_auth_jll
